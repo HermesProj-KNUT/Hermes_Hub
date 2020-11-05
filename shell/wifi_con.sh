c6a8b4bc24eef12584904c2e97d3ac6713ea8b0f
용시훈 <tlgns1537@naver.com>
2020-11-05 11:29:22 +0900

    hub_setting_update

--- shell/wifi_con.sh
+++ shell/wifi_con.sh
@@ -6,3 +6,6 @@ sudo wpa_passphrase $1 $2 >> /etc/wpa_supplicant/wpa_supplicant.conf
 wpa_cli -i wlan0 reconfigure
 sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
 sudo iw wlan0 link
+sudo iw dev wlan0 set power_save off
+sudo wpa_supplicant -c/etc/wpa_supplicant/wpa_supplicant.conf -iwlan0 -d
+wpa_supplicant -c/etc/wpa_supplicant/wpa_supplicant.conf -iwlan0 -d
